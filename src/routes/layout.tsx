37cab54d35d0a2192a66f91c171f8448e62ce3e6
A2-NieR <36411819+A2-NieR@users.noreply.github.com>
2024-12-26 22:16:43 +0100

    feat: disable caching for admin routes

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,15 +1,24 @@
 import { component$, Slot } from "@builder.io/qwik";
 import { type RequestHandler } from "@builder.io/qwik-city";
 
-export const onGet: RequestHandler = async ({ cacheControl }) => {
+export const onGet: RequestHandler = async ({ cacheControl, url }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
   // https://qwik.dev/docs/caching/
-  cacheControl({
-    // Always serve a cached response by default, up to a week stale
-    staleWhileRevalidate: 60 * 60 * 24 * 7,
-    // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
-    maxAge: 5,
-  });
+  if (url.pathname.includes("/admin")) {
+    cacheControl({
+      public: false,
+      maxAge: 0,
+      sMaxAge: 0,
+      staleWhileRevalidate: 0,
+    });
+  } else {
+    cacheControl({
+      // Always serve a cached response by default, up to a week stale
+      staleWhileRevalidate: 60 * 60 * 24 * 7,
+      // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
+      maxAge: 5,
+    });
+  }
 };
 
 export default component$(() => {
