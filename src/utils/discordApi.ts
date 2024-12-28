612eaa6c9201dde99c1d735cc59c0f632997de04
A2-NieR <36411819+A2-NieR@users.noreply.github.com>
2024-12-28 18:34:50 +0100

    revert: add global types

--- src/utils/discordApi.ts
+++ src/utils/discordApi.ts
@@ -1,30 +1,3 @@
-interface Guild {
-  id: string;
-  name: string;
-  icon: string | null;
-  banner: string | null;
-  owner: boolean;
-  permissions: number;
-  permissions_new: string;
-  features: Array<string>;
-}
-
-interface Channel {
-  id: string;
-  type: number;
-  last_message_id: string;
-  flags: number;
-  last_pin_timestamp: string;
-  guild_id: string;
-  name: string;
-  parent_id: string;
-  rate_limit_per_user: number;
-  topic: null | string;
-  position: number;
-  permission_overwrites: Array<any>;
-  nsfw: boolean;
-}
-
 export async function fetchUserInfo(accessToken: string) {
   const response = await fetch("https://discord.com/api/v10/users/@me", {
     headers: {
