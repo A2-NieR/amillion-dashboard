e89d6ff5dcc16051e1a44baf522109a24a3fd76b
A2-NieR <36411819+A2-NieR@users.noreply.github.com>
2024-12-30 19:47:27 +0100

    refactor: remove dashboard button component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,11 @@
 import { component$ } from "@builder.io/qwik";
-import DashboardButton from "./dashboard-button";
+import { useSession, useSignIn } from "~/routes/plugin@auth";
+import { Form } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const session = useSession();
+  const signInSig = useSignIn();
+
   return (
     <nav class="navbar bg-base-100 shadow">
       <div class="flex-1">
@@ -10,7 +14,17 @@ export default component$(() => {
         </a>
       </div>
       <div class="flex-none">
-        <DashboardButton />
+        {session.value ? (
+          <a class="btn btn-outline" href="/admin">
+            Dashboard
+          </a>
+        ) : (
+          <Form action={signInSig}>
+            <input type="hidden" name="providerId" value="discord" />
+            <input type="hidden" name="options.redirectTo" value="/admin" />
+            <button class="btn btn-outline">Dashboard</button>
+          </Form>
+        )}
       </div>
     </nav>
   );
